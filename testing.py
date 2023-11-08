d4561e6f6c5d204e4f44c68f41f9613d1151a839
Joao Borges <joaoborges@hotmail.com>
2023-11-08 08:37:23 -0300

    string tips

--- testing.py
+++ testing.py
@@ -1,11 +1,17 @@
-while True:
+# while True:
 
-    m = int(input('type a number: '))
-    print('\n')
-    for i in range(1,11):
-        print(f'{m} x {i} = {m*i}')
-        i += 1
+#     m = int(input('type a number: '))
+#     print('\n')
+#     for i in range(1,11):
+#         print(f'{m} x {i} = {m*i}')
+#         i += 1
 
-    print('\n')
+#     print('\n')
+#     break
 
+#String Tips
+name = 'John'
+print('=' * 20)
+print(f'{name:-^20}')
+print('=' * 20)
 
